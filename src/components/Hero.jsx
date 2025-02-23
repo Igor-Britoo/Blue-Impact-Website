bebc41c7b9803ce855f057e6932701ca76872d72
tiaobraw <tiaobraw@gmail.com>
2025-02-23 11:13:16 -0300

    feat: add popup component for additional information display

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,25 +1,45 @@
-import React, { forwardRef } from 'react'
-import { HeroSection, Logo, HeroTitle, HeroSubtitle, LearnMoreButton } from './styled/HeroComponents'
+import React, { useState, useEffect, forwardRef } from 'react'
+import { HeroSection, Logo, HeroTitle, HeroSubtitle, LearnMoreButton, Popup, Card, CardContent } from './styled/HeroComponents'
 import logo from '/hero-logo.svg'
+import heroCardImage from '/hero_saibamais_card.png'
 
 const Hero = forwardRef(({ faqRef, ...props }, ref) => {
-  const scrollToSection = (ref) => {
-    if (ref.current) {
-      ref.current.scrollIntoView({ behavior: 'smooth' });
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+  const togglePopup = () => {
+    setIsPopupOpen(!isPopupOpen);
+  };
+
+  const closePopup = (e) => {
+    if (e.target === e.currentTarget) {
+      setIsPopupOpen(false);
     }
-  }
+  };
+
+  useEffect(() => {
+    if (isPopupOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+  }, [isPopupOpen]);
 
   return (
     <HeroSection ref={ref} {...props}>
       <Logo src={logo} alt="Blue Impact Logo" />
       <HeroSubtitle>
-        <span>Lorem Ipsum is simply dummy text of the printing and</span>
-        typesetting industry. Lorem Ipsum has been the industry's
-        standard dummy text.
+        <strong>Ciência e Tecnologia para um Oceano Sustentável</strong>
       </HeroSubtitle>
-      <LearnMoreButton onClick={() => scrollToSection(faqRef)}>
+      <LearnMoreButton onClick={togglePopup}>
         SAIBA MAIS
       </LearnMoreButton>
+      <Popup className={isPopupOpen ? 'open' : ''} onClick={closePopup}>
+        <Card style={{ backgroundImage: `url(${heroCardImage})` }}>
+          <CardContent>
+            Aqui está o textão do card.
+          </CardContent>
+        </Card>
+      </Popup>
     </HeroSection>
   )
 })
